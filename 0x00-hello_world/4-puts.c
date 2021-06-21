060d42632a9fc12ee553e4e1ae985abd153c7a43
Munyat <briankipkiruimunyat@gmail.com>
2021-06-20 22:39:22 -0700

    print using put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,6 +9,6 @@ int main(void)
 	/**
 	 * this code prints the line below
 	 */
-	puts("\"Programming is like bilding a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
