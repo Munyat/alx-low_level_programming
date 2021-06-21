e05452eb6f74a6a2ff3be84f7748b37ac9f3ac37
Munyat <briankipkiruimunyat@gmail.com>
2021-06-20 22:35:52 -0700

    print using put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main -entry for c coding
+* in lowlevel coding section.
+* Return: 0
+*/
+int main(void)
+{
+	/**
+	 * this code prints the line below
+	 */
+	puts("\"Programming is like bilding a multilingual puzzle");
+	return (0);
+}
